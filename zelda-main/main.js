53c1eb3a195762b8709f95f388be6f2f5dee96b1
dk <dayka6601@gmail.com>
2024-04-15 16:20:49 +0200

    Déplacement avec le clavier

--- zelda-main/main.js
+++ zelda-main/main.js
@@ -96,3 +96,18 @@ function avancerTop()
 
 	return false;
 }
+
+
+// déplacer le personnage avec le clavier 
+
+document.addEventListener('keydown', (event) => {
+	if(event.key === 'ArrowUp'){
+		avancerTop();
+	}else if (event.key === 'ArrowDown') {
+		avancerBottom();
+	}else if (event.key === 'ArrowLeft'){
+		avancerLeft();
+	}else if (event.key === 'ArrowRight'){
+		avancerRight();
+	}
+})
