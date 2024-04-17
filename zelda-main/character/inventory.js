a2b79365ed790ef2a252dcf8ed9f8b2783608bcb
Toma <devulder.thomas2005@gmail.com>
2024-04-17 15:25:25 +0200

    update and fix inventory

--- zelda-main/character/inventory.js
+++ zelda-main/character/inventory.js
@@ -1,30 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let inventoryButton = document.getElementById("inventoryButton");
-    let inventoryDiv = document.getElementById("inventoryfull");
-    let overlay = document.getElementById("overlay");
+    var inventoryButton = document.getElementById("inventoryButton");
+    var inventoryDiv = document.getElementById("inventoryfull");
+    var overlay = document.getElementById("overlay");
 
+    // Ajoutez un écouteur d'événements pour détecter les pressions de touche
     document.addEventListener("keydown", function(event) {
+        // Vérifiez si la touche "Tab" a été pressée
         if (event.key === "Tab") {
+            // Empêche le comportement par défaut de la touche "Tab" (navigation par défaut)
             event.preventDefault();
             toggleInventory();
         }
     });
 
+    // Ajoutez un écouteur d'événements pour détecter le clic sur le bouton
     inventoryButton.addEventListener("click", function(event) {
-        event.stopPropagation(); // Empêche la propagation pour éviter que le clic soit pris en compte
+        event.stopPropagation(); // Empêche la propagation de l'événement pour éviter que le clic ne soit pris en compte par le document
         toggleInventory();
     });
 
-    
+    // Ajoutez un écouteur d'événements pour détecter les clics sur l'ensemble du document
     document.addEventListener("click", function(event) {
-        let target = event.target; // L'élément sur lequel le clic a été effectué
+        var target = event.target; // L'élément sur lequel le clic a été effectué
 
-        
+        // Vérifiez si l'inventaire est affiché et si l'élément cliqué n'est ni l'inventaire ni son bouton de contrôle
         if (inventoryDiv.style.display !== "none" && target !== inventoryDiv && target !== inventoryButton) {
             toggleInventory(); // Fermez l'inventaire
         }
     });
 
+    // Fonction pour afficher ou masquer l'inventaire et l'overlay
     function toggleInventory() {
         if (inventoryDiv.style.display === "none") {
             inventoryDiv.style.display = "block";
