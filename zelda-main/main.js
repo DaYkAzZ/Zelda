b1f0da02aeb4f9fac37f72489f78c9bef431e2e4
dk <dayka6601@gmail.com>
2024-04-17 11:01:54 +0200

    Nouvelle Map

--- zelda-main/main.js
+++ zelda-main/main.js
@@ -1,111 +1,21 @@
-  
+let zelda = document.getElementById('zelda')
+let chest = document.getElementById("chest");
 
-function avancerRight()
-{
-	var zelda=document.getElementById("zelda");
-	x=zelda.style.left;
-	
-	
-	if(zelda.className=="dirDroite1")
-	{
-		zelda.className="dirDroite2";
-		zelda.style.left=parseInt(x)+10+"px";
-	}
-	else if(zelda.className=="dirDroite2")
-	{
-		zelda.className="dirDroite3";
-		zelda.style.left=parseInt(x)+10+"px";
-	}
-	else
-	{
-		zelda.className="dirDroite1";
-		zelda.style.left=parseInt(x)+10+"px";
-	}
+function setCoords(e, type) {
+  let idX = type + "X";
+  let idY = type + "Y";
 
-	return false;
+  document.getElementById(idX).innerText = e[idX];
+  document.getElementById(idY).innerText = e[idY];
 }
 
-function avancerLeft()
-{
-	var zelda=document.getElementById("zelda");
-	x=zelda.style.left;
-	
-	if(zelda.className=="dirGauche1")
-	{
-		zelda.className="dirGauche2";
-		zelda.style.left=parseInt(x)-10+"px";
-	}
-	else if(zelda.className=="dirGauche2")
-	{
-		zelda.className="dirGauche3";
-		zelda.style.left=parseInt(x)-10+"px";
-	}
-	else
-	{
-		zelda.className="dirGauche1";
-		zelda.style.left=parseInt(x)-10+"px";
-	}
-
-	return false;
+function update(e) {
+  setCoords(e, "offset");
+  setCoords(e, "client");
+  setCoords(e, "page");
+  setCoords(e, "screen");
 }
 
-function avancerBottom()
-{
-	var zelda=document.getElementById("zelda");
-	y=zelda.style.top;
-	
-	if(zelda.className=="dirBas1")
-	{
-		zelda.className="dirBas2";
-		zelda.style.top=parseInt(y)+10+"px";
-	}
-	else if(zelda.className=="dirBas2")
-	{
-		zelda.className="dirBas3";
-		zelda.style.top=parseInt(y)+10+"px";
-	}
-	else
-	{
-		zelda.className="dirBas1";
-		zelda.style.top=parseInt(y)+10+"px";
-	}
-
-	return false;
-}
-
-function avancerTop()
-{
-	var zelda=document.getElementById("zelda");
-	y=zelda.style.top;
-	
-	if(zelda.className=="dirHaut1")
-	{
-		zelda.className="dirHaut2";
-		zelda.style.top=parseInt(y)-10+"px";
-	}
-	else if(zelda.className=="dirHaut2")
-	{
-		zelda.className="dirHaut3";
-		zelda.style.top=parseInt(y)-10+"px";
-	}
-	else
-	{
-		zelda.className="dirHaut1";
-		zelda.style.top=parseInt(y)-10+"px";
-	}
-
-	return false;
-}
-
-// capacité ultime 
-
-function ulti() {
-	let zelda = document.getElementById('zelda');
-	zelda.style.background = "url(images/boutons.png) 150px 150px";
-}
-
-document.addEventListener('keydown', (event)  => {
-	if(event.key === 'U'){
-		ulti();
-	}
-})
+inner.addEventListener("mouseenter", update, false);
+inner.addEventListener("mousemove", update, false);
+inner.addEventListener("mouseleave", update, false);
