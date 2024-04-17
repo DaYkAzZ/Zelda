db8bf2069e7ff82f372013af91445d3884caed17
Toma <devulder.thomas2005@gmail.com>
2024-04-17 20:29:03 +0200

    Update main.js

--- zelda-main/main.js
+++ zelda-main/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('mousemove', function(event) {
-     let x = event.clientX;
-     let y = event.clientY;
+    let x = event.clientX;
+    let y = event.clientY;
 
-    console.log('Coordonnées de la souris - X : ' + x + ', Y : ' + y);
+   console.log('Coordonnées de la souris - X : ' + x + ', Y : ' + y);
 });
